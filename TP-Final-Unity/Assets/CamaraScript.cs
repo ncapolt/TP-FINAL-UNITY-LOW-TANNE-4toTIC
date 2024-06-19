a94a0b16bd9033d5414aa0d7587e6be8fcfc0fa2
ncapolt <140573792+ncapolt@users.noreply.github.com>
2024-06-19 09:24:42 -0300

    iuasisadu

---
+++ TP-Final-Unity/Assets/CamaraScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraScript : MonoBehaviour
+{
+    public GameObject [] Objetos;
+    public int indice = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i > Objetos.Length; i++)
+        {
+            Objetos[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Objetos[indice].SetActive(false);
+            indice++;
+                if (indice == Objetos.Length)
+            {
+                indice = 0;
+            }
+            Objetos[indice].SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Objetos[indice].SetActive(false);
+            indice--;
+            if (indice == -1)
+            {
+                indice = Objetos.Length - 1;
+            }
+            Objetos[indice].SetActive(true);
+        }
+    }
+}
